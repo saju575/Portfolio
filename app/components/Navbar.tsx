f366383157bec8ceca3ec64e9d00d8ad193ea720
saju575 <sakilahmedsaju575@gmail.com>
2023-07-08 20:15:08 +0600

    Project initialize

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const Navbar = () => {
+  return <></>;
+};
+
+export default Navbar;
---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="dark:bg-gray-700">
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
