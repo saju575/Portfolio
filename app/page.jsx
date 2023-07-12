18de2e219eaf7327be7501eaef28795af48a63ce
saju575 <sakilahmedsaju575@gmail.com>
2023-07-12 22:27:56 +0600

    url change

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Header from "./components/header/Header";
-// import Projects from "./components/projects/Projects";
+import Projects from "./components/projects/Projects";
 import Qualification from "./components/qualification/Qualification";
 
 // const inter = Inter({ subsets: ["latin"] });
@@ -15,7 +15,7 @@ export default function Home() {
 
       <About />
       <Qualification />
-      {/* <Projects /> */}
+      <Projects />
       <Contact />
     </>
   );
