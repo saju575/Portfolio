505408b3f6e3b5bb9628f4fcd05d06101fd6d7d4
saju575 <sakilahmedsaju575@gmail.com>
2023-07-13 13:33:58 +0600

    resume change

--- app/components/qualification/Skill.jsx
+++ app/components/qualification/Skill.jsx
@@ -54,7 +54,7 @@ const Skill = () => {
                 <div className="p-4 bg-[#151F28] text-[#8998AC] rounded shadow flex space-x-3">
                   <div>
                     <p className="mb-3 ml-2 text-[17px] font-semibold">
-                      Comfortable
+                      Comfortable with
                     </p>
                     <div className="flex flex-wrap">
                       {shields
@@ -124,11 +124,45 @@ const Skill = () => {
                 <div className="p-4 bg-[#151F28] text-[#8998AC] rounded shadow flex space-x-3">
                   <div>
                     <p className="mb-3 ml-2 text-[17px] font-semibold">
-                      Tools and Technologies Used
+                      Tools and Software
                     </p>
                     <div className="flex flex-wrap">
                       {shields
                         .filter((shield) => shield.category === "tools")
+                        .map((s) => {
+                          return (
+                            <img
+                              className="m-2"
+                              src={s.img}
+                              alt={s.alt}
+                              key={s.id}
+                            />
+                          );
+                        })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="rounded-full bg-blue-500 border-white border-4 w-8 h-8 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
+              {/* <i className="text-white fa-solid fa-brain"></i> */}
+              <FontAwesomeIcon className="text-white" icon={faBrain} />
+            </div>
+          </div>
+        </div>
+        {/* <!-- Left section, set by justify-start and sm:pr-8 --> */}
+        <div className="mt-6 sm:mt-0 sm:mb-12">
+          <div className="flex flex-col sm:flex-row items-center">
+            <div className="flex justify-start w-full mx-auto items-center">
+              <div className="w-full sm:w-1/2 sm:pr-8">
+                <div className="p-4 bg-[#151F28] text-[#8998AC] rounded shadow flex space-x-3">
+                  <div>
+                    <p className="mb-3 ml-2 text-[17px] font-semibold">
+                      Version Control
+                    </p>
+                    <div className="flex flex-wrap">
+                      {shields
+                        .filter((shield) => shield.category === "vControl")
                         .map((s) => {
                           return (
                             <img
